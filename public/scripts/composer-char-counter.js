603406adbbb40ef944d8966abb6f2a4bb33c2f1c
Jeremy Buist <jbuistjbuist@gmail.com>
2022-10-11 16:16:50 -0400

    fix typo

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
   form.on('input', function() {
     let twtLength = $(this).val().length
     let counter = $(this).parent().find(".counter"); 
-    console.log(counter);
 
     if (twtLength <= 140) {
       $(counter).removeClass("red").html(140 - twtLength);
