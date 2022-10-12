f026c267a7742d0067faa7927d9072e5ae38bf5a
Jeremy Buist <jbuistjbuist@gmail.com>
2022-10-12 16:54:32 -0400

    adding form validation to reject too long and null inputs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
   const createTweetElement = function(tweetData) {
 
-    time = timeago.format(tweetData.created_at);
+    let time = timeago.format(tweetData.created_at);
     
     let $tweet = $('<article><header class="tweetheader"></header><p></p><footer></footer></article>');
 
@@ -22,7 +22,7 @@ $(document).ready(function() {
 const renderTweets = function(tweets) {
   tweets.forEach(tweet => {
     let $el = createTweetElement(tweet);
-    $('.tweetlist').append($el);
+    $('.tweetlist').prepend($el);
   })
 }
 
@@ -35,8 +35,27 @@ loadTweets();
 $('.tweetform').submit(function(event) {
   event.preventDefault();
   const data = $(this).serialize();
+  const textLength = $('#tweet-text').val().length;
 
-  $.post('/tweets', data);
+  if (!textLength) {
+    alert("Please write something in order to make a tweet!");
+    return;
+  }
+
+  if (textLength > 140) {
+    alert("Your text exceeds 140 characters.")
+    return;
+  }
+
+  $(this).trigger('reset');
+
+  $.post('/tweets', data).then(() => {
+    $.get('/tweets').then((data) => {
+      let $tweet = createTweetElement(data[data.length - 1]);
+      $('.tweetlist').prepend($tweet);
+    }).catch(error => console.log(error))
+
+  });
 
 })
 
