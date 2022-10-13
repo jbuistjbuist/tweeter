23679d498027457e4d52ae16da203cffc6824bd3
Jeremy Buist <jbuistjbuist@gmail.com>
2022-10-12 20:06:27 -0400

    adding effect to make the nav elements more visible on scroll

--- public/scripts/stretch-navigation.js
+++ public/scripts/stretch-navigation.js
@@ -11,6 +11,17 @@ $(document).ready(function(){
     $('#tweet-text').trigger("focus");
   })
 
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop()
+      if (scroll >= 500) {
+        $('nav > div').addClass("nav-el-bg")
+      }
+      if (scroll <= 500) {
+        $('nav > div').removeClass("nav-el-bg")
+      }
+  })
+
 })
 
-///reference: https://www.abeautifulsite.net/posts/smoothly-scroll-to-an-element-without-a-jquery-plugin-2
+///references: https://www.abeautifulsite.net/posts/smoothly-scroll-to-an-element-without-a-jquery-plugin-2
+//https://stackoverflow.com/questions/12558311/add-remove-class-with-jquery-based-on-vertical-scroll
