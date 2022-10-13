a9bf8e420736abff0e49107fde36536bc525529b
Jeremy Buist <jbuistjbuist@gmail.com>
2022-10-12 23:12:27 -0400

    changing header size

--- public/scripts/stretch-navigation.js
+++ public/scripts/stretch-navigation.js
@@ -25,12 +25,12 @@ $(document).ready(function(){
 
   $(window).scroll(function() {
       var scroll = $(window).scrollTop()
-      if (scroll >= 450) {
+      if (scroll >= 330) {
         $('.logo').addClass("nav-el-bg");
         navbtn.fadeOut(200);
         scrollbtn.fadeIn(100)
       }
-      if (scroll < 450) {
+      if (scroll < 330) {
         $('.logo').removeClass("nav-el-bg");
         navbtn.fadeIn(200);
         scrollbtn.fadeOut(200);
